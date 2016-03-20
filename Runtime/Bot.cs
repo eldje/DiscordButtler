c8c17076eaca60775068fab441cc91120f15d7c9
Mathias Storm <mathiasstorm1@gmail.com>
2016-03-20 11:44:06 +0100

    Adding base classses

---
+++ Runtime/Bot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace DiscordButtler.Runtime
+{
+    public static class Bot
+    {
+        public static AuthCredentials BotCredentials { get; } = new AuthCredentials();
+
+        private static DiscordClient client = new DiscordClient();
+
+        public static async Task<bool> Run()
+        {
+            await client.Connect(BotCredentials.Email, BotCredentials.Password);
+            HookLogging();
+            return true;
+        }
+
+        private static void HookLogging()
+        {
+            
+        }
+
+    }
+}
