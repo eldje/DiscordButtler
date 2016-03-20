d6ce874b30d41c30b559ba989193632a3026d2e2
Mathias Storm <mathiasstorm1@gmail.com>
2016-03-20 13:46:14 +0100

    Adding Interface for extensions

---
+++ Extensions/IExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands.Permissions.Visibility;
+
+namespace DiscordButtler.Extensions
+{
+    public interface IExtension
+    {
+        void Setup();
+        string CommandDomain { get; set; }
+    }
+}
